fdce10ad746a4cf5f9f6498faacfff7dcf78ee8b
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-02-25 21:25:48 +0200

    5.13 ja 5.14

---
+++ Osa_5/bloglist-frontend/src/components/SimpleBlog.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SimpleBlog = ({ blog, onClick }) => (
+  <div>
+    <div className='titleauthor'>
+      {blog.title} {blog.author}
+    </div>
+    <div className='likes'>
+      blog has {blog.likes} likes
+      <button className='like' onClick={onClick}>like</button>
+    </div>
+  </div>
+)
+
+export default SimpleBlog
