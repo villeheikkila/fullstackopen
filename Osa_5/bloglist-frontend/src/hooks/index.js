d7c7a571ea9220336f3d0b9d2538e70c83c9dec0
Ville Heikkilä <mail@villeheikkila.com>
2019-02-25 22:06:03 +0200

    5.18 ja 5.19

--- Osa_5/bloglist-frontend/src/hooks/index.js
+++ Osa_5/bloglist-frontend/src/hooks/index.js
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 
-export const useField = (type) => {  const [value, setValue] = useState('')
+export const useField = (type) => {
+  const [value, setValue] = useState('')
 
   const onChange = (event) => {
     setValue(event.target.value)
   }
 
+  const reset = () => {
+    setValue('')
+  }
+
   return {
     type,
     value,
-    onChange
+    onChange,
+    reset
   }
 }
