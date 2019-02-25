f6185e2b7cff1ff233c9887adefd3dee3b7c6456
Ville Heikkilä <mail@villeheikkila.com>
2019-02-25 23:32:17 +0200

    5.8

--- Osa_5/bloglist-frontend/src/App.js
+++ Osa_5/bloglist-frontend/src/App.js
@@ -111,7 +111,7 @@ const App = () => {
 
   const blogForm = () => (
     <div >
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
