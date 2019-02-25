18e6c07dab65da68bce1a22344bb28bb8ab1fb13
Ville Heikkilä <mail@villeheikkila.com>
2019-02-25 23:20:19 +0200

    5.6 ja CSS import

--- Osa_5/bloglist-frontend/src/components/Blog.js
+++ Osa_5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        <div onClick={() => setVisible(true)}>
+          {blog.title} {blog.author}
+        </div>
+      </div>
+      <div style={showWhenVisible}>
+        <div onClick={() => setVisible}>
+          {blog.title} <br />
+          {blog.author} <br />
+          {blog.url} <br />
+          {blog.likes}
+          <button type="submit">like</button>
+        </div>
+      </div>
+    </div>
+
+  )
+}
 
 export default Blog
--- Osa_5/bloglist-frontend/src/components/Notification.js
+++ Osa_5/bloglist-frontend/src/components/Notification.js
@@ -1,14 +1,15 @@
 import React from 'react'
+
 const Notification = ({ message }) => {
-    if (message === null) {
-      return null
-    }
-  
-    return (
-      <div className="error">
-        {message}
-      </div>
-    )
+  if (message === null) {
+    return null
   }
 
-export default Notification;
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
+export default Notification
