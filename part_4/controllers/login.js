142cfee7314e4eab101c5190749d0371f0d8731b
Ville Heikkilä <mail@villeheikkila.com>
2019-06-13 02:39:22 +0300

    Exercise 4.20

--- part_4/controllers/login.js
+++ part_4/controllers/login.js
@@ -5,11 +5,13 @@ const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
   const body = request.body
+  console.log('body : ', body)
 
   const user = await User.findOne({ username: body.username })
-  const passwordCorrect = user === null
-    ? false
-    : await bcrypt.compare(body.password, user.passwordHash)
+  const passwordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash)
 
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
@@ -19,14 +21,12 @@ loginRouter.post('/', async (request, response) => {
 
   const userForToken = {
     username: user.username,
-    id: user._id,
+    id: user._id
   }
 
   const token = jwt.sign(userForToken, process.env.SECRET)
 
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name })
+  response.status(200).send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
--- part_4/utils/middleware.js
+++ part_4/utils/middleware.js
@@ -26,8 +26,17 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
