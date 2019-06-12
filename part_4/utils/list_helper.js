21602187626d75ec50fd0e0bd50ed658944cf7eb
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-06-13 01:58:36 +0300

    Exercises 4.6 and 4.7

--- part_4/utils/list_helper.js
+++ part_4/utils/list_helper.js
@@ -9,8 +9,43 @@ const favoriteBlog = blogs => {
   return blogs.reduce((a, b) => (a.likes > b.likes ? a : b))
 }
 
+const mostBlogs = blogs => {
+  const result = blogs.reduce((a, b) => {
+    let known = a.find(found => {
+      return found.author === b.author
+    })
+
+    if (!known) {
+      return a.concat({ author: b.author, blogs: 1 })
+    }
+
+    known.blogs++
+    return a
+  }, [])
+
+  return result.reduce((a, b) => (a.blogs > b.blogs ? a : b))
+}
+
+const mostLikes = blogs => {
+  const result = blogs.reduce((a, b) => {
+    let known = a.find(found => {
+      return found.author === b.author
+    })
+
+    if (!known) {
+      return a.concat({ author: b.author, likes: b.likes })
+    }
+
+    known.likes += b.likes
+    return a
+  }, [])
+
+  return favoriteBlog(result)
+}
 module.exports = {
   dummy,
   totalLikes,
+  mostLikes,
+  mostBlogs,
   favoriteBlog
 }
