298bbe85e9c86585b1cc662d90456f296fce563c
Ville Heikkilä <mail@villeheikkila.com>
2019-06-14 10:53:24 +0300

    Add skeletons for remaining exercises

---
+++ part_6/redux-anecdotes/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const App = (props) => {
+  const anecdotes = props.store.getState()
+  
+  const vote = (id) => {
+    console.log('vote', id)
+  }
+  
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ part_6/redux-anecdotes/src/components/Notification.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Notification = () => {
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+  return (
+    <div style={style}>
+      render here notification...
+    </div>
+  )
+}
+
+export default Notification
