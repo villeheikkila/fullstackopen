c695ea4390b5fcc2ccb49e503a066634ac893230
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-07-01 20:06:22 +0300

    Exercise 6.16

---
+++ part_6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    return response.data
+}
+
+export default { getAll }
