afdfb6495a264e532d4958d48e0441d420d9126f
Ville Heikkilä <mail@villeheikkila.com>
2019-07-01 20:06:22 +0300

    Exercise 6.16

--- part_6/redux-anecdotes/src/index.js
+++ part_6/redux-anecdotes/src/index.js
@@ -4,8 +4,15 @@ import { createStore } from 'redux'
 import App from './App'
 import reducer from './reducers/reducer'
 import { Provider } from 'react-redux'
+import anecdotesService from './services/anecdotes'
 
 const store = createStore(reducer)
+
+anecdotesService.getAll().then(anecdotes => anecdotes.forEach(anecdote => {
+  store.dispatch({ type: 'NEW_ANECDOTE', data: anecdote }
+  )
+}))
+
 
 const render = () => {
   ReactDOM.render(
