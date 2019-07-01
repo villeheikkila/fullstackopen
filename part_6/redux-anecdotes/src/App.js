8811afe17f59fa9ec9a126fc603e3cfb94af2fc6
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-07-01 17:31:46 +0300

    Exercise 6.8

--- part_6/redux-anecdotes/src/App.js
+++ part_6/redux-anecdotes/src/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
-import { vote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = (props) => {
-  const anecdotesInitial = props.store.getState()
-  const anecdotes = [...anecdotesInitial].sort((a, b) => {
-    return b.votes - a.votes
-  })
-
-  const voteId = (id) => {
-    props.store.dispatch(vote(id))
-  }
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => voteId(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList store={props.store} />
       <AnecdoteForm store={props.store} />
     </div>
   )
---
+++ part_6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { vote } from '../reducers/anecdoteReducer'
+
+
+const AnecdoteList = (props) => {
+    const anecdotesInitial = props.store.getState()
+    const anecdotes = [...anecdotesInitial].sort((a, b) => {
+        return b.votes - a.votes
+    })
+
+    const voteId = (id) => {
+        props.store.dispatch(vote(id))
+    }
+
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => voteId(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default AnecdoteList
