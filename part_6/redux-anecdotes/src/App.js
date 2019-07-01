3666b6bc83fcfaf49b396541838fae3b701c2fa5
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-07-01 20:19:07 +0300

    Move initializing from index.js to App.js

--- part_6/redux-anecdotes/src/App.js
+++ part_6/redux-anecdotes/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import anecdotesService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
-const App = () => {
+
+const App = (props) => {
+  useEffect(() => {
+    anecdotesService.getAll().then(anecdotes =>
+      props.initializeAnecdotes(anecdotes)
+    )
+  }, [props])
+
   return (
     <div>
       <h1>Anecdotes</h1>
@@ -16,4 +26,4 @@ const App = () => {
   )
 }
 
-export default App
+export default connect(null, { initializeAnecdotes })(App)
--- part_6/redux-anecdotes/src/index.js
+++ part_6/redux-anecdotes/src/index.js
@@ -4,15 +4,8 @@ import { createStore } from 'redux'
 import App from './App'
 import reducer from './reducers/reducer'
 import { Provider } from 'react-redux'
-import anecdotesService from './services/anecdotes'
 
 const store = createStore(reducer)
-
-anecdotesService.getAll().then(anecdotes => anecdotes.forEach(anecdote => {
-  store.dispatch({ type: 'NEW_ANECDOTE', data: anecdote }
-  )
-}))
-
 
 const render = () => {
   ReactDOM.render(
