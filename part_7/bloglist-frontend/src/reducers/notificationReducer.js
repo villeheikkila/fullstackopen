b18e7dc4e7d1cd52acd5644982e6e7567d4e0669
Ville Heikkilä <mail@villeheikkila.com>
2019-07-02 02:36:50 +0300

    Exercise 7.4

---
+++ part_7/bloglist-frontend/src/reducers/notificationReducer.js
@@ -0,0 +1,33 @@
+
+export const createNotification = (content, time) => {
+    return async dispatch => {
+        dispatch({
+            type: 'SET_NOTIFICATION',
+            data: { message: content.message, type: content.type }
+        })
+        setTimeout(() => {
+            dispatch({
+                type: 'DELETE_NOTIFICATION',
+            })
+        }, time * 1000)
+    }
+}
+
+export const deleteNotification = () => {
+    return {
+        type: 'DELETE_NOTIFICATION',
+    }
+}
+
+const notificationReducer = (state = { message: "Hei", type: "Success" }, action) => {
+    switch (action.type) {
+        case 'SET_NOTIFICATION':
+            return action.data
+        case 'DELETE_NOTIFICATION':
+            return ""
+        default:
+            return state
+    }
+}
+
+export default notificationReducer
