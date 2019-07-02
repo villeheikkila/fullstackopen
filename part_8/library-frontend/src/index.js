2adecef53d837a0bf5728b60eaba8a4cadc0ef41
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-07-02 23:24:11 +0300

    Unfished work, issues with CORS...

--- part_8/library-frontend/src/index.js
+++ part_8/library-frontend/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient, { gql } from 'apollo-boost'
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+})
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <ApolloProvider client={client} >
+        <App />
+    </ApolloProvider>
+    , document.getElementById('root'))
