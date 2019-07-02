c370b7c735bd7c6bcd84ececad132db65294678e
Ville Heikkilä <ville.heikkila@helsinki.fi>
2019-07-02 23:48:59 +0300

    Exercise 8.8

--- part_8/library-frontend/src/App.js
+++ part_8/library-frontend/src/App.js
@@ -3,12 +3,13 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import { gql } from 'apollo-boost'
-import { Query } from 'react-apollo'
+import { Query, ApolloConsumer } from 'react-apollo'
 
 const ALL_AUTHORS = gql`
 {
   allAuthors {
     name
+    born
     bookCount
   }
 }
@@ -16,36 +17,38 @@ const ALL_AUTHORS = gql`
 
 const App = () => {
   const [page, setPage] = useState('authors')
-  return <Query query={ALL_AUTHORS}>
-    {(result) => {
-      if (result.loading) {
-        return <div>loading...</div>
-      }
-      return (
-        <div>
-          <div>
-            <button onClick={() => setPage('authors')}>authors</button>
-            <button onClick={() => setPage('books')}>books</button>
-            <button onClick={() => setPage('add')}>add book</button>
-          </div>
 
-          <Authors
-            show={page === 'authors'}
-          />
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
 
-          <Books
-            show={page === 'books'}
-          />
+      <ApolloConsumer>
+        {(client =>
+          <Query query={ALL_AUTHORS}>
+            {(result) =>
+              <Authors
+                show={page === 'authors'} result={result} client={client}
+              />
+            }
+          </Query>
+        )}
+      </ApolloConsumer>
 
-          <NewBook
-            show={page === 'add'}
-          />
 
-        </div>
-      )
-    }}
-  </Query>
+      <Books
+        show={page === 'books'}
+      />
 
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
 }
 
 export default App
