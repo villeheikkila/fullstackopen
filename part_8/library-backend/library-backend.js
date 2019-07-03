fd2893db3d6e176ba33280d9c7753ab2171c5bdf
Ville Heikkilä <mail@villeheikkila.com>
2019-07-03 15:55:26 +0300

    Exercise 8.16

--- part_8/library-backend/library-backend.js
+++ part_8/library-backend/library-backend.js
@@ -1,11 +1,13 @@
-const { ApolloServer, gql, UserInputError } = require('apollo-server')
+const { ApolloServer, gql, UserInputError, AuthenticationError } = require('apollo-server')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const Book = require('./models/book')
 const Author = require('./models/author')
 
 mongoose.set('useFindAndModify', false)
+
 const MONGODB_URI = process.env.MONGODB_URI
+const JWT_SECRET = process.env.JWT_SECRET
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
     .then(() => {
@@ -16,6 +18,14 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
     })
 
 const typeDefs = gql`
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+  type Token {
+    value: String!
+  }
   type Author {
     name: String!
     id: ID!
@@ -37,6 +47,7 @@ const typeDefs = gql`
     authorCount: String!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
 
   type Mutation {
@@ -49,7 +60,15 @@ const typeDefs = gql`
     editAuthor(    
         name: String!    
         setBornTo: Int!  
-    ): Author
+    ): Author,
+    createUser(
+        username: String!
+        favoriteGenre: String!
+    ): User
+    login(
+        username: String!
+        password: String!
+  ): Token
   }
 `
 
@@ -69,7 +88,10 @@ const resolvers = {
         },
         allAuthors: () => {
             return Author.find({})
-        }
+        },
+        me: (root, args, context) => {
+            return context.currentUser
+        },
     },
     Author: {
         bookCount: (root) => {
@@ -77,7 +99,13 @@ const resolvers = {
         }
     },
     Mutation: {
-        addBook: async (root, args) => {
+        addBook: async (root, args, context) => {
+            const currentUser = context.currentUser
+
+            if (!currentUser) {
+                throw new AuthenticationError("not authenticated")
+            }
+
             const authorExists = await Author.findOne({ name: args.author })
 
             if (authorExists === null) {
@@ -103,7 +131,13 @@ const resolvers = {
                 })
             }
         },
-        editAuthor: async (root, args) => {
+        editAuthor: async (root, args, context) => {
+            const currentUser = context.currentUser
+
+            if (!currentUser) {
+                throw new AuthenticationError("not authenticated")
+            }
+
             const author = await Author.findOne({ name: args.name })
             author.born = args.setBornTo
 
@@ -118,7 +152,31 @@ const resolvers = {
                     invalidArgs: args,
                 })
             }
-        }
+        },
+        createUser: (root, args) => {
+            const user = new User({ username: args.username })
+
+            return user.save()
+                .catch(error => {
+                    throw new UserInputError(error.message, {
+                        invalidArgs: args,
+                    })
+                })
+        },
+        login: async (root, args) => {
+            const user = await User.findOne({ username: args.username })
+
+            if (!user || args.password !== 'secret') {
+                throw new UserInputError("wrong credentials")
+            }
+
+            const userForToken = {
+                username: user.username,
+                id: user._id,
+            }
+
+            return { value: jwt.sign(userForToken, JWT_SECRET) }
+        },
     }
 }
 
@@ -129,6 +187,14 @@ const server = new ApolloServer({
     },
     typeDefs,
     resolvers,
+    context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.toLowerCase().startsWith('bearer ')) {
+            const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
+            const currentUser = await User.findById(decodedToken.id).populate('friends')
+            return { currentUser }
+        }
+    }
 })
 
 server.listen().then(({ url }) => {
---
+++ part_8/library-backend/models/user.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3
+    }
+})
+
+module.exports = mongoose.model('User', schema)
