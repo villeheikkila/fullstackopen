e8c75ebbf0dafa28c5db8b0acf56c135bff8cd74
Ville Heikkilä <mail@villeheikkila.com>
2021-03-20 15:25:00 +0200

    Exercises 9.10-9.11

---
+++ part_9/patientor-backend/data/patients.json
@@ -0,0 +1,42 @@
+[
+  {
+    "id": "d2773336-f723-11e9-8f0b-362b9e155667",
+    "name": "John McClane",
+    "dateOfBirth": "1986-07-09",
+    "ssn": "090786-122X",
+    "gender": "male",
+    "occupation": "New york city cop"
+  },
+  {
+    "id": "d2773598-f723-11e9-8f0b-362b9e155667",
+    "name": "Martin Riggs",
+    "dateOfBirth": "1979-01-30",
+    "ssn": "300179-77A",
+    "gender": "male",
+    "occupation": "Cop"
+  },
+  {
+    "id": "d27736ec-f723-11e9-8f0b-362b9e155667",
+    "name": "Hans Gruber",
+    "dateOfBirth": "1970-04-25",
+    "ssn": "250470-555L",
+    "gender": "male",
+    "occupation": "Technician"
+  },
+  {
+    "id": "d2773822-f723-11e9-8f0b-362b9e155667",
+    "name": "Dana Scully",
+    "dateOfBirth": "1974-01-05",
+    "ssn": "050174-432N",
+    "gender": "female",
+    "occupation": "Forensic Pathologist"
+  },
+  {
+    "id": "d2773c6e-f723-11e9-8f0b-362b9e155667",
+    "name": "Matti Luukkainen",
+    "dateOfBirth": "1971-04-09",
+    "ssn": "090471-8890",
+    "gender": "male",
+    "occupation": "Digital evangelist"
+  }
+]
--- part_9/patientor-backend/index.ts
+++ part_9/patientor-backend/index.ts
@@ -1,14 +1,54 @@
 import express from "express";
 import cors from "cors";
+
+import diagnosesJSON from "./data/diagnoses.json";
+import patientsJSON from "./data/patients.json";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const PORT = 3001;
 
+interface Diagnose {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+interface Patient {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: string;
+  occupation: string;
+}
+
 app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
+});
+
+app.get("/api/diagnoses", (_req, res) => {
+  const diagnoses: Diagnose[] = diagnosesJSON;
+  res.send(diagnoses);
+});
+
+const getNonSensitiveEntriesFromPatient = (
+  patients: Patient[]
+): Omit<Patient, "ssn">[] =>
+  patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+
+app.get("/api/patients", (_req, res) => {
+  const patients: Patient[] = patientsJSON;
+  res.send(getNonSensitiveEntriesFromPatient(patients));
 });
 
 app.listen(PORT, () => {
